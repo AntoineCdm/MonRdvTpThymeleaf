0ead66ed799b81646343d98e150ba0c95c53ead7
DUBOURG <jasondubourg@gmail.com>
2019-04-04 14:16:31 +0200

    Jason 8

--- mon-rdv-spring-mvc/formation-web-spring-th/src/main/java/sopra/formation/controller/LoginController.java
+++ mon-rdv-spring-mvc/formation-web-spring-th/src/main/java/sopra/formation/controller/LoginController.java
@@ -1,69 +1,69 @@
-package sopra.formation.controller;
-
-
-
-import javax.servlet.http.Cookie;
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import sopra.formation.monRdv.model.Utilisateur;
-import sopra.formation.monRdv.repository.IUtilisateur;
-
-
-@Controller
-@RequestMapping("/login")
-public class LoginController {
-	
-	@Autowired
-	private IUtilisateur utilisateurRepo;
-	
-	@GetMapping("")
-	public String defaut() {
-<<<<<<< HEAD
-		return "redirect:/login/login";
-=======
-		return "redirect:/login/list";
->>>>>>> master
-	}
-	
-	@GetMapping(path = { "/", "/list" })
-	public String list(Model model) {
-		List<Utilisateur> utilisateurs = utilisateurRepo.findAll();
-
-		model.addAttribute("mesUtilisateurs", utilisateurs);
-
-		return "login/list";
-	}
-	
-	@PostMapping("/sinscrire")
-	public String sinscrire(@Valid @ModelAttribute("monUtilisateur") Utilisateur utilisateur, BindingResult result) {
-		if(result.hasErrors()) {
-			return "login/login";
-		}
-		
-		utilisateurRepo.save(utilisateur);
-
-		return "redirect:/home";
-	}
-	
-	@PostMapping("/sidentifier")
-	public String sidentifier(@Valid @ModelAttribute("monUtilisateur") Utilisateur utilisateur, BindingResult result) {
-		if(result.hasErrors()) {
-			return "login/login";
-		}
-		
-		Cookie cookie = new Cookie("type", utilisateur.getType().toString());
-
-		
-		return "redirect:/home";
-	}
-
-}
+//package sopra.formation.controller;
+//
+//
+//
+//import javax.servlet.http.Cookie;
+//import javax.validation.Valid;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import sopra.formation.monRdv.model.Utilisateur;
+//import sopra.formation.monRdv.repository.IUtilisateur;
+//
+//
+//@Controller
+//@RequestMapping("/login")
+//public class LoginController {
+//	
+//	@Autowired
+//	private IUtilisateur utilisateurRepo;
+//	
+//	@GetMapping("")
+//	public String defaut() {
+//<<<<<<< HEAD
+//		return "redirect:/login/login";
+//=======
+//		return "redirect:/login/list";
+//>>>>>>> master
+//	}
+//	
+//	@GetMapping(path = { "/", "/list" })
+//	public String list(Model model) {
+//		List<Utilisateur> utilisateurs = utilisateurRepo.findAll();
+//
+//		model.addAttribute("mesUtilisateurs", utilisateurs);
+//
+//		return "login/list";
+//	}
+//	
+//	@PostMapping("/sinscrire")
+//	public String sinscrire(@Valid @ModelAttribute("monUtilisateur") Utilisateur utilisateur, BindingResult result) {
+//		if(result.hasErrors()) {
+//			return "login/login";
+//		}
+//		
+//		utilisateurRepo.save(utilisateur);
+//
+//		return "redirect:/home";
+//	}
+//	
+//	@PostMapping("/sidentifier")
+//	public String sidentifier(@Valid @ModelAttribute("monUtilisateur") Utilisateur utilisateur, BindingResult result) {
+//		if(result.hasErrors()) {
+//			return "login/login";
+//		}
+//		
+//		Cookie cookie = new Cookie("type", utilisateur.getType().toString());
+//
+//		
+//		return "redirect:/home";
+//	}
+//
+//}
