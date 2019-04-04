9bfab212999032f060d906228d135fda405bdffc
Gary <bouchentouf.gary@gmail.com>
2019-04-04 14:29:15 +0200

    add layout menu in pratician search result & home

--- mon-rdv-spring-mvc/formation-web-spring-th/src/main/java/sopra/formation/controller/HomeController.java
+++ mon-rdv-spring-mvc/formation-web-spring-th/src/main/java/sopra/formation/controller/HomeController.java
@@ -1,13 +1,22 @@
 package sopra.formation.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sopra.formation.monRdv.model.Praticien;
+import sopra.formation.monRdv.repository.IPraticien;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
+	private IPraticien praticienRepo;
 	
 	@GetMapping("")
 	public String defaut() {
@@ -22,6 +31,11 @@ public class HomeController {
 	
 	@GetMapping("pratician-list")
 	public String getPratician(Model model) {
+		
+		List<Praticien> praticiens = praticienRepo.findAll();
+
+		model.addAttribute("mesPraticiens", praticiens);
+
 
 		return "/search-pratician/pratician-list";
 	}
