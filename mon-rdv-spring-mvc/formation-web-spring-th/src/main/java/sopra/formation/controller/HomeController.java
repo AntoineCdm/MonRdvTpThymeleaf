cae7c41933a3bf37520cf2b572c2554dbc3ebbad
loic <loic.dederen@gmail.com>
2019-04-05 15:45:36 +0200

    recherche

--- mon-rdv-spring-mvc/formation-web-spring-th/src/main/java/sopra/formation/controller/HomeController.java
+++ mon-rdv-spring-mvc/formation-web-spring-th/src/main/java/sopra/formation/controller/HomeController.java
@@ -1,12 +1,15 @@
 package sopra.formation.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import sopra.formation.monRdv.model.Praticien;
 import sopra.formation.monRdv.repository.IPraticien;
@@ -14,35 +17,34 @@ import sopra.formation.monRdv.repository.IPraticien;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	@Autowired
 	private IPraticien praticienRepo;
-	
+
 	@GetMapping("")
 	public String defaut() {
 		return "redirect:/home";
 	}
-	
-	@GetMapping(path = {"/home" })
+
+	@GetMapping(path = { "/home" })
 	public String home(Model model) {
 
 		return "home";
 	}
-	
-	@GetMapping("pratician-list")
-	public String searchPraticien(Model model) {
-		
+
+	@PostMapping("pratician-list")
+	public String searchPraticien(Model model, @RequestParam("nom") String nom) {
+
 		List<Praticien> praticiens_tmp = praticienRepo.findAll();
-		List<Praticien> praticiens = praticienRepo.findAll();
-		
-		for( Praticien praticien: praticiens_tmp) {
-			if(praticien.getNom().matches("(.*)Caumes(.*)"));
-			praticiens.add(praticien);
+		List<Praticien> praticiens = new ArrayList<Praticien>();
+
+		for (Praticien praticien : praticiens_tmp) {
+			if (praticien.getNom().matches("(.*)"+nom+"(.*)")) {
+				praticiens.add(praticien);
+			}
 		}
-		
+
 		model.addAttribute("mesPraticiens", praticiens);
-
-
 
 		return "/search-pratician/pratician-list";
 	}
